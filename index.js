1043d4df77e911eb64b7072fe648d48335d5de16
Kris Zima <kris@mosquito.ie>
2015-11-30 19:56:33 +1000

    add decodeEntities to support single quotes in html snippets

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
 var cheerio = require('cheerio');
 var hl = require('highlight.js');
+var he = require('he');
 
 module.exports = function(input) {
     this && this.cacheable && this.cacheable();
-    var $ = cheerio.load(input);
+    var $ = cheerio.load(input, {decodeEntities: false});
     $('code').each(function (index, code) {
         var lang = $(code).attr('class');
-         if (lang && hl.getLanguage(lang)) {
+        console.log($(code).html())
+        if (lang && hl.getLanguage(lang)) {
             $(code).html(hl.highlight(lang, $(code).html()).value);
         } else {
             $(code).html(hl.highlightAuto($(code).html()).value);
